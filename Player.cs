eefa26c9cdb6f032e5cb2d01f4c1af971e967c71
Andrey Morozov <andre15@inbox.ru>
2018-12-01 04:05:02 +0400

    IsBustingGhost hotfix

--- Player.cs
+++ Player.cs
@@ -171,14 +171,15 @@ namespace Klee
                             {
                                 Console.WriteLine($"BUST {hunterBustingGhost.Id}");
                                 isBusting = true;
+                                continue;
                             }
-                            else
+                            if (!IsBustingGhost(hunterBustingGhost) || buster.State == 4)
                             {
                                 var movingPoint = GetBustTrapPointNew(buster.Point, hunterBustingGhost, false, false);
                                 Console.WriteLine($"MOVE {movingPoint.X} {movingPoint.Y} {hunterBustingGhost.Id}");
+                                continue;
                             }
-
-                            continue;
+                            
                         }
 
                         //загоняем призраков
